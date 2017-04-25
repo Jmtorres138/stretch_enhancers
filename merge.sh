5d767e37cde92dd6f68eeca6889ef28a2ddbf052
Jason Torres <jmtorres@rescomp1.(none)>
2017-04-25 18:39:36 +0100

    added merge files

---
+++ merge.sh
@@ -0,0 +1,2 @@
+/apps/well/bedtools/2.24.0/mergeBed -i Pancreat_islet_15_dense.en-only.bed > islet_merged_enhancers.bed
+
---
+++ merge2.sh
@@ -0,0 +1,1 @@
+/apps/well/bedtools/2.24.0/mergeBed -d 4200 -i Pancreat_islet_15_dense.en-only.perc90.bed > Pancreat_stretch-enhancers.bed 
